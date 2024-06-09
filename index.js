bbbd90e1c5bee3de62539f82e8eb28c859e0bf43
Mst Tahmida Begum Jakia <devtahmidajakia@gmail.com>
2024-06-08 23:01:54 -0700

    created a apartment post method api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 
@@ -161,6 +162,27 @@ async function run() {
     app.get('/apartmentCount', async(req,res) => {
       const count = await apartmentCollection.estimatedDocumentCount()
       res.send({count})
+    })
+
+
+
+    // payment intent
+
+    app.post('/create-payment-intent', async(req,res) => {
+      const {price} = req.body;
+    const amount = parseInt(price * 100)
+    console.log(amount,'amount inside')
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: 'usd',
+      payment_method_types: ['card']
+    })
+    
+    res.send({
+      clientSecret: paymentIntent.client_secret
+    })
+
     })
 
 
