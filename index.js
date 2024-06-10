3100d175c6d03b840917b428a846e57dd2dc8dc8
Mst Tahmida Begum Jakia <devtahmidajakia@gmail.com>
2024-06-10 03:27:35 -0700

    create post apartment ap

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://hotel-apartment.web.app",
+      "https://hotel-apartment.firebaseapp.com",
+    ]
+  })
+);
 app.use(express.json());
 
 
@@ -26,7 +34,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 
     const apartmentCollection = client.db("buildingDb").collection("apartment");
@@ -107,10 +115,22 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/agreements', async(req,res) => {
+    // app.get('/agreements', async(req,res) => {
+    //   const email = req.query.email;
+    //   const query = {email: email};
+    //   const result = await agreementCollection.find(query).toArray();
+    //   res.send(result);
+    // });
+    app.get('/agreements/:email', async(req,res) => {
       const email = req.query.email;
       const query = {email: email};
       const result = await agreementCollection.find(query).toArray();
+      res.send(result);
+    });
+
+
+    app.get('/agreements', async(req,res) => {
+      const result = await agreementCollection.find().toArray();
       res.send(result);
     });
 
@@ -143,7 +163,7 @@ async function run() {
     })
 
     app.get('/announcements', async(req,res) => {
-      const result = await announcementCollection.find().toArray();
+      const result = await announcementCollection.find().toArray()
       res.send(result);
     });
 
@@ -187,6 +207,7 @@ async function run() {
     });
 
 
+
     app.post('/payments', async(req,res) => {
       const payment= req.body;
       const paymentResult = await paymentCollection.insertOne(payment)
@@ -216,7 +237,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
